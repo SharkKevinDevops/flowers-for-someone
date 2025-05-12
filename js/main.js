16e3e8ce34a9d584ea53af572ef37ef7d1074302
SharkKevinDevops <phuccloud86@gmail.com>
2025-05-12 21:35:48 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('Dear Gia Nhi, wishing you a life where every step is light, every dream is bright, and every moment is wrapped in love.').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
